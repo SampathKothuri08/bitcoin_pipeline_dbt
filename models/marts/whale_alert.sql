1ac19c3784fc77f7d9c04f0926b1d7ef4e54e902
sampath <sampathsvrk@gmail.coml>
2026-01-06 09:59:51 -0500

    Added usd conversion for bitcoin price data

--- models/marts/whale_alert.sql
+++ models/marts/whale_alert.sql
@@ -1,4 +1,5 @@
 
+with whales as (
 select 
 
 output_address,
@@ -11,3 +12,20 @@ where output_value > 10
 
 group by output_address
 order by total_sent desc
+),
+latest_price as (
+    select 
+    *
+    from 
+    {{ ref("btc_usd_max")}}
+    where to_date(replace(snapped_at,'UTC','')) = current_date()
+)
+
+select 
+w.output_address,
+w.total_sent,
+w.txn_count,
+l.price,
+(l.price * w.total_sent) as total_sent_usd
+from whales w cross join latest_price l 
+order by total_sent_usd desc
