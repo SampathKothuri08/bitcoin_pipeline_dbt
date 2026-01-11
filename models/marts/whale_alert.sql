cc860beb1ff0d060808ac80ea4dfa134d23fe849
sampath <sampathsvrk@gmail.coml>
2026-01-10 18:34:51 -0800

    added a macro

--- models/marts/whale_alert.sql
+++ models/marts/whale_alert.sql
@@ -1,7 +1,6 @@
 
 with whales as (
 select 
-
 output_address,
 sum(output_value) as total_sent,
 count(*) as txn_count 
@@ -12,20 +11,11 @@ where output_value > 10
 
 group by output_address
 order by total_sent desc
-),
-latest_price as (
-    select 
-    *
-    from 
-    {{ ref("btc_usd_max")}}
-    where to_date(replace(snapped_at,'UTC','')) = current_date()
 )
-
 select 
 w.output_address,
 w.total_sent,
 w.txn_count,
-l.price,
-(l.price * w.total_sent) as total_sent_usd
-from whales w cross join latest_price l 
+{{ convert_to_usd('w.total_sent')}} as total_sent_usd
+from whales w 
 order by total_sent_usd desc
